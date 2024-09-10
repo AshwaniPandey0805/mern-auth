93bca0021932dcb783106afad388e2a28856dca4
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-09-10 20:04:56 +0530

    conneted to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const port = 3000;
+console.log(process.env.MONGODB);
+mongoose.connect(process.env.MONGODB).then(()=>{
+    console.log('connected to mongoDB')
+}).catch((error) => {
+    console.log(error)
+});
+
 app.listen(port, ()=>{
     console.log(`Server is running on prot ${port} number`);
 });
