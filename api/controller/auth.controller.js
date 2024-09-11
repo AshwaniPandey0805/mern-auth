f94a28891c15134885bf7470be5f0509266a59c7
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-09-11 20:18:06 +0530

    created signin API route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,10 @@
 import { errorHandler } from "../errors/error.js";
 import User from "../model/user.model.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import bc from "bcrypt"
+dotenv.config();
 
 export const signUp = async  (req, res, next ) => {
     const { username, email, password } = req.body;
@@ -14,4 +18,34 @@ export const signUp = async  (req, res, next ) => {
     } catch (error) {
         next(errorHandler(300, error.message));
     }
+}
+
+export const signIn = async  ( req, res, next) => {
+    const { email, password } = req.body;
+    // res.json({
+    //     email,
+    //     password
+    // });
+    try {
+        const validateUser = await  User.findOne({ email });
+        if(!validateUser){
+            return next(errorHandler(401, "User not found"));
+        }
+        const validatePassword = bcrypt.compareSync(password, validateUser.password);
+        if(!validatePassword){
+            return next(errorHandler(401, 'wrong credentials'));
+        }
+        const token = jwt.sign({ id : validateUser._id }, process.env.SECRET);
+        const { password : hashedPassword, ...rest  } = validateUser._doc;
+        const tokenExpiry = new Date(Date.now() + 3600000); // 1 hour
+        res.cookie('access_token', token, {
+            httpOnly : true,
+            expires : tokenExpiry
+        })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+        next(error);
+    }
+
 }
